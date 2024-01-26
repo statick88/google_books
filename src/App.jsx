a26cea88b6b613212e59c342e02a7250fef4a1d4
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2024-01-26 15:18:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 // App.jsx
-import { Route, Routes } from 'react-router-dom';
-import SearchBooks from './components/SearchBooks';
-import Home from './components/Home';
-import './App.css'; // Asegúrate de importar tu archivo de estilos aquí
+import { Route, Routes, Router} from "react-router-dom";
+import SearchBooks from "./components/SearchBooks";
+import Home from "./components/Home";
+import "./App.css"; // Asegúrate de importar tu archivo de estilos aquí
 
 const App = () => {
   return (
-    <div className="app-container"> {/* Agregamos una clase para el fondo oscuro */}
-      <h1>Buscador de libros</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchBooks />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchBooks />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
